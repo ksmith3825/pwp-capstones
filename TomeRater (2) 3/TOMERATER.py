f48f1289dbdd9d2aeec776e92fd442c7c6cff616
ksmith3825 <40182531+ksmith3825@users.noreply.github.com>
2018-08-25 21:45:57 -0400

    Codecademy generated from the project submission page.

---
+++ TomeRater (2) 3/TOMERATER.py
@@ -0,0 +1,120 @@
+class User(object):
+    def __init__(self, name, email):
+        name = ""
+        email = ""
+        self.book = {}
+            
+    def get_email(self):
+        email = self
+        return email
+
+    def change_email(self, address):
+        new_email = self + address
+        return "this user's email has been updated"
+
+    def __repr__(self):
+        return self + adress
+
+    def __eq__(self, other_user):
+        return self = other_user
+
+    def read_book(book, rating):
+        return self.books
+
+    def get_average_rating(self.books):
+        return average
+    
+class Book:
+    def book(self, title, isbn):
+        self.title = title
+        self.isbn = isbn
+        self.ratings = []
+
+    def get_title(title):
+        return title
+
+    def get_isbn(isbn):
+        return isbn
+
+    def set_isbn(isbn, new_isbn):
+        if isbn = new_isbn:
+            return "this book's ISBN has been updated"
+
+    def add_rating(rating, self.rating):
+        if rating > 0 and rating < 5:
+            return self.rating + rating += self.rating
+        else:
+            return "Invalid Rating"
+        
+    def __eq__(book):
+        return book = book
+    
+    def get_average_rating(self.ratings):
+        return average
+    
+    def __hash__(self):
+        return hash((self.title, self.isbn))
+    
+class Fiction(Book):
+    def __init__(self, title, author, isbn):
+        self.author = title + isbn
+        def get_author(author):
+            return author
+        def __repr__(string):
+            return string
+
+class NonFiction(Book):
+    def __init__(self, title, subject, level, isbn):
+        self.subject = subject
+        self.level = level
+        def get_string(subject):
+            return subject
+        def get_level(level):
+            return level
+        def __repr__(string):
+            return string
+
+class TomeRater:
+    def user_numbers(self):
+        self.users = {}
+        return adress
+
+    def create_book(title, isbn):
+        new_book = title + isbn
+        return new_book
+
+    def create_novel(title, author, isbn):
+        new_fiction = title + author + isbn
+        return new_fiction
+
+    def create_non_fiction(title, subject, level, isbn):
+        new_non_fiction = title + author + isbn
+        return new_non_fiction
+
+    def add_book_to_user(book, email, rating):
+        rating = none
+        if self.user = email:
+            self.user(read_book)
+        else:
+            return "No user with email {email}!"
+
+    def add_user(name, email, books):
+        books = none
+        new_user = name + email
+        return new_user
+
+    def print_catalog():
+        return print(self.books)
+
+    def print_users():
+        return print(self.users)
+
+    def most_read_book():
+        return self.books
+
+    def highest_rated_book():
+        return book.get_average_rating()
+
+    def most_positive_user():
+        return user
+    
